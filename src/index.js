aa0d02a515f1f333b1c26235492983c23f18f013
Ksenia Nesytova <alleheldentot@gmail.com>
2022-07-01 19:38:16 +0700

    FIX: Webpack now compiles PUG without issue

--- src/index.js
+++ src/index.js
@@ -1,10 +1,1 @@
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
+console.log("working");
